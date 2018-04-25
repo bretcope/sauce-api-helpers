19b91d9da376b8802f3aad5f4f132ca3851b208d
Bret Copeland <bret@atlantisflight.org>
2018-04-25 15:52:12 -0400

    Fix missing object name

--- usage-by-user.js
+++ usage-by-user.js
@@ -103,7 +103,7 @@ async function getAllUsernamesFromParent(parentUsername, auth)
     {
         if (user.children_count > 0)
         {
-            usernames = usernames.concat(await getAllUsernamesFromParent(username, auth));
+            usernames = usernames.concat(await getAllUsernamesFromParent(user.username, auth));
         }
         else
         {
